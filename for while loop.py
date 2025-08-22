b887d86915f24d3e301ee8732792579f18f15982
sangeetha425 <valugubellysangeethareddy@gmail.com>
2025-08-22 15:52:24 +0530

    Add files via upload

---
+++ for while loop.py
@@ -0,0 +1,22 @@
+# n=100
+# for i in range(1,n+1):
+#     if i&1==0:
+#         print(i) #to print even numbers
+
+# n=6
+# sum=1
+# for i in range(1,21):
+#      sum+=i
+#      print(n,'x',i,'=',n*i)
+
+# n=50
+# sum=0
+# for i in range(1,n+1):
+#     sum +=i
+#     print(sum) #sum of first n natural numbers.
+
+# n=int(input('enter value'))
+# factorial=1
+# for i in range(1,n+1):
+#     factorial*=i
+#     print(factorial)
